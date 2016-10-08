fcdfef1398f4687951330ba852ddd5b706a39d17
EvaSky <olga.shahray@gmail.com>
2016-10-08 16:50:10 +0200

    resources update

--- web/resources/js/custom.js
+++ web/resources/js/custom.js
@@ -1,10 +1,10 @@
 // Script to open and close sidenav
-function open() {
+function sidenav_open() {
     document.getElementById("mySidenav").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
 
-function close() {
+function sidenav_close() {
     document.getElementById("mySidenav").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
